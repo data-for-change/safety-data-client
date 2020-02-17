42cf427dbe11a2ddf2ae8eafc6ac6516fce94d15
citizen-dror <reshef.dror@gmail.com>
2020-02-17 17:43:42 +0200

    split to filter panel and request

---
+++ src/components/FilterPanel.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import Collapse from 'react-bootstrap/Collapse';
+import { FilterRequest } from './FilterRequest';
+
+
+export function FilterPanel() {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <>
+            <Button
+                onClick={
+                    () => {
+                        setOpen(!open);
+                    }}
+                aria-controls="example-collapse-text"
+                aria-expanded={open}
+            >
+                Options
+        </Button>
+            <Collapse in={open}>
+                <div id="example-collapse-text">
+                   <FilterRequest />
+                </div>
+            </Collapse>
+        </>
+    );
+}
+
--- src/components/FilterRequest.jsx
+++ src/components/FilterRequest.jsx
@@ -1,60 +1,41 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Button from 'react-bootstrap/Button';
-import Collapse from 'react-bootstrap/Collapse';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col'
 
 export function FilterRequest() {
-    const [open, setOpen] = useState(false);
-
     return (
-        <>
-            <Button
-                onClick={
-                    () => {
-                        setOpen(!open);
-                    }}
-                aria-controls="example-collapse-text"
-                aria-expanded={open}
-            >
-                Options
-        </Button>
-            <Collapse in={open}>
-                <div id="example-collapse-text">
-                    <Form>
-                    <Form.Row>
-                        <Form.Group as={Col} controlId="exampleForm.ControlSelectStartYear">
-                            <Form.Label>Start Year</Form.Label>
-                            <Form.Control as="select">
-                                <option>2015</option>
-                                <option>2016</option>
-                                <option>2017</option>
-                                <option>2018</option>
-                                <option>2019</option>
-                            </Form.Control>
-                        </Form.Group>
-                        <Form.Group as={Col} controlId="exampleForm.ControlSelectEndYear">
-                            <Form.Label>End Year</Form.Label>
-                            <Form.Control as="select">
-                                <option>2015</option>
-                                <option>2016</option>
-                                <option>2017</option>
-                                <option>2018</option>
-                                <option>2019</option>
-                            </Form.Control>
-                        </Form.Group>
-                    </Form.Row>
-                        <Form.Group controlId="exampleForm.ControlCity">
-                            <Form.Label>City</Form.Label>
-                            <Form.Control type="input" placeholder="חיפה" />
-                        </Form.Group>
-                        <Button variant="primary"  onClick={() => {console.log("get accident");}} >
-                            Do Filter
-                        </Button>
-                    </Form>
-                </div>
-            </Collapse>
-        </>
+        <Form>
+        <Form.Row>
+            <Form.Group as={Col} controlId="exampleForm.ControlSelectStartYear">
+                <Form.Label>Start Year</Form.Label>
+                <Form.Control as="select" onChange={(e) => {console.log(e.target.value);}}>
+                    <option>2015</option>
+                    <option>2016</option>
+                    <option>2017</option>
+                    <option>2018</option>
+                    <option>2019</option>
+                </Form.Control>
+            </Form.Group>
+            <Form.Group as={Col} controlId="exampleForm.ControlSelectEndYear">
+                <Form.Label>End Year</Form.Label>
+                <Form.Control as="select">
+                    <option>2015</option>
+                    <option>2016</option>
+                    <option>2017</option>
+                    <option>2018</option>
+                    <option>2019</option>
+                </Form.Control>
+            </Form.Group>
+        </Form.Row>
+            <Form.Group controlId="exampleForm.ControlCity">
+                <Form.Label>City</Form.Label>
+                <Form.Control type="input" placeholder="חיפה" />
+            </Form.Group>
+            <Button variant="primary"  onClick={() => {console.log("get accident");}} >
+                Do Filter
+            </Button>
+        </Form>
     );
 }
 
