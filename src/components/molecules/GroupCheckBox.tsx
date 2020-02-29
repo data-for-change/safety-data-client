b2d707d8e9e27c28ed1a03e29ae095ffbaf694b5
citizen-dror <reshef.dror@gmail.com>
2020-02-29 17:03:51 +0200

    fix key problem

--- src/components/molecules/GroupCheckBox.tsx
+++ src/components/molecules/GroupCheckBox.tsx
@@ -1,28 +1,28 @@
 
-import React,{ ChangeEvent} from 'react'
+import React, { ChangeEvent } from 'react'
 import { useTranslation } from 'react-i18next';
-import { observer } from "mobx-react"
+//import { observer } from "mobx-react"
 import Form from 'react-bootstrap/Form';
-import {Checkbox} from '../atoms/Checkbox';
-import {IFilterChecker} from '../../stores/FilterChecker'
+import { Checkbox } from '../atoms/Checkbox';
+import { IFilterChecker } from '../../stores/FilterChecker'
 //import { useStore } from '../../stores/storeConfig'
 
 interface IProps {
-    formName:string,
-    groupName: string,
-    dataArr: Array<IFilterChecker>,
-    onChange:(aType: number, val: boolean) => void
-  }
+  formName: string,
+  groupName: string,
+  dataArr: Array<IFilterChecker>,
+  onChange: (aType: number, val: boolean) => void
+}
 
-export const GroupCheckbox: React.FC<IProps> =  observer(({formName,groupName,dataArr,onChange}) => {
-    const { t } = useTranslation();
-    //const store = useStore();
-    return (
-        <Form.Group controlId={formName+".Control"+groupName} >
-        <Form.Label className="filterLable">{t(groupName)}:</Form.Label>
-        {dataArr.map((fChecker, index) => (
-				<Checkbox label={fChecker.label} group={groupName} id={index} checked={fChecker.checked} onChange={(e: ChangeEvent<HTMLInputElement>) => { onChange(index, e.target.checked); }}/>
-			))}
-      </Form.Group>
-    )
-})
+export const GroupCheckbox: React.FC<IProps> = ({ formName, groupName, dataArr, onChange }) => {
+  const { t } = useTranslation();
+  //const store = useStore();
+  return (
+    <Form.Group controlId={formName + ".Control" + groupName} >
+      <Form.Label className="filterLable">{t(groupName)}:</Form.Label>
+      {dataArr.map((fChecker, index) => (
+        <Checkbox key={index} label={fChecker.label} group={groupName} id={index} checked={fChecker.checked} onChange={(e: ChangeEvent<HTMLInputElement>) => { onChange(index, e.target.checked); }} />
+      ))}
+    </Form.Group>
+  )
+}
