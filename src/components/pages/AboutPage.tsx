bfdf21945ea947a672bbd002a0f36c6cf9953c71
citizen-dror <reshef.dror@gmail.com>
2020-03-05 14:15:45 +0200

    add noopener noreferrer to links

--- src/components/pages/AboutPage.tsx
+++ src/components/pages/AboutPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 interface IProps { }
 
@@ -14,13 +14,13 @@ export const AboutPage: React.FC<IProps> = () => {
       <div style={styleDiv}> Safety Data מציג נתונים על תאונות דרכים, בדגש על נפגעים בתאונות קשות וקטלניות.
 האתר מתמקד בניתוח בתאונות קטלניות, מיקוד זה הוא על פי "חזון אפס הרוגים בתאונות דרכים" ששואף להביא לאפס את הנפגעים בתאונות קשות וקטלניות.
 </div>
-<div style={styleDiv}>
-      האתר מציע מגוון אפשרויות לבצע חיתוכים של הנתונים, לקבץ אותם ולהציגם בצורות שונות, בתקווה שדבר זה יסייע להבנה טובה יותר למניעת תאונות. 
+      <div style={styleDiv}>
+        האתר מציע מגוון אפשרויות לבצע חיתוכים של הנתונים, לקבץ אותם ולהציגם בצורות שונות, בתקווה שדבר זה יסייע להבנה טובה יותר למניעת תאונות.
 </div>
-<div style={styleDiv}>
+      <div style={styleDiv}>
         כך תוכלו לראות לדוגמה
 </div>
-<div style={styleDiv}>
+      <div style={styleDiv}>
         <ul>
           <li>מה הבדלים בין היפגעות נשים וגברים, וגם במה שונים כלי הרכב שבהן נשים נפגעות לעומת גברים.</li>
           <li> באיזה מקומות נפגעים נהגי מכוניות ומה סוגי התאונות הקטלניות השכיחות ביותר – בעיר ומחוץ לעיר.</li>
@@ -29,7 +29,7 @@ export const AboutPage: React.FC<IProps> = () => {
         </ul>
       </div>
       <div style={styleDiv}>
-        מקור הנתונים על התאונות הוא הלמ"ס , דרך מנוע הנתונים של פרויקט <a href={'https://www.anyway.co.il'} target="_blank">Anyway</a>  של הסדנא לידע ציבורי. המפה באתר היא תודות לפרויקט <a href={'https://www.openstreetmap.org/'} target="_blank">OpenStreetMap</a>.
+        מקור הנתונים על התאונות הוא הלמ"ס , דרך מנוע הנתונים של פרויקט <a href={'https://www.anyway.co.il'} target="_blank" rel="noopener noreferrer">Anyway</a>  של הסדנא לידע ציבורי. המפה באתר היא תודות לפרויקט <a href={'https://www.openstreetmap.org/'} target="_blank" rel="noopener noreferrer">OpenStreetMap</a>.
 </div>
 
       {/* <Footer /> */}
