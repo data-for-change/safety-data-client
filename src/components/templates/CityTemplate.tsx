aa100c622bfbbe23857fffc8f100cac280d3a2dc
citizen-dror <reshef.dror@gmail.com>
2020-03-06 09:38:08 +0200

    default filter Card in citypage is where

--- src/components/templates/CityTemplate.tsx
+++ src/components/templates/CityTemplate.tsx
@@ -24,7 +24,7 @@ export const CityTemplate: React.FC<IProps> = observer(() => {
     <div className="App">
       <div className="container-fluid">
         <div className="row ">
-          <div className="p-2 col-md-2"><FilterPanel /></div>
+          <div className="p-2 col-md-2"><FilterPanel activeCardKey={1}/></div>
           <div className="col-md-10">
               <h4>{cityResult}</h4>
               <TabsTemplate defaultKey="map"/>
