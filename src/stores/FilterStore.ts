34e3706f5455810fbf84ff1ff0a1c6a5c1742f73
citizen-dror <reshef.dror@gmail.com>
2020-03-17 20:19:07 +0200

    `submintMainDataFilter

--- src/stores/FilterStore.ts
+++ src/stores/FilterStore.ts
@@ -6,7 +6,7 @@ import * as FC from './FilterChecker'
 import * as GroupBy from './GroupBy'
 import { fetchFilter, fetchGroupBy } from "../services/Accident.Service"
 import CityService from '../services/City.Service'
-import {demoIDb} from '../services/Injured.idb.Service'
+import { setMulti } from '../services/Injured.idb.Service'
 //import autorun  from "mobx"
 
 export default class FilterStore {
@@ -27,7 +27,7 @@ export default class FilterStore {
     FC.initSeparator(this.separator);
     FC.initOneLane(this.oneLane);
     GroupBy.initGroupByDict(this.groupByDict);
-    GroupBy.initGroup2Dict (this.group2Dict)
+    GroupBy.initGroup2Dict(this.group2Dict)
     this.groupBy = this.groupByDict["TypeInjured"];
     this.groupBy2 = this.group2Dict["Gender"];
     this.appInitialized = false;
@@ -38,8 +38,8 @@ export default class FilterStore {
   updateInjurySeverity = (aType: number, val: boolean) => {
     this.injurySeverity[aType].checked = val;
   }
-//////////////////////////////////////////////
-  
+  //////////////////////////////////////////////
+
 
   // this belong to root store
   @observable
@@ -274,8 +274,7 @@ export default class FilterStore {
     //console.log(filter)
     fetchGroupBy(filter)
       .then((data: any[] | undefined) => {
-        if (data !== undefined)
-        {
+        if (data !== undefined) {
           let fixData = this.groupBy2.fixStrcutTable(data)
           //let fixData = this.fixStrcutTable(data)
           this.dataGroupby2 = fixData;
@@ -283,9 +282,9 @@ export default class FilterStore {
       })
   }
   @observable
-  groupBy2 :GroupBy.GroupBy2;
+  groupBy2: GroupBy.GroupBy2;
   @action
-  updateGroupBy2 =(key:string) =>{
+  updateGroupBy2 = (key: string) => {
     this.groupBy2 = this.group2Dict[key]
     this.submitfilterdGroup2(this.groupBy, this.groupBy2.name);
   }
@@ -299,7 +298,7 @@ export default class FilterStore {
     if (groupName2 === "")
       filter += ',{"$group": { "_id": "$' + groupName + '", "count": { "$sum": 1 }}}';
     else {
-      filter +=', { "$match" : { "'+groupName2+'" : { "$exists" : true, "$ne" : null}}}'
+      filter += ', { "$match" : { "' + groupName2 + '" : { "$exists" : true, "$ne" : null}}}'
       let grpids = '{ "grp1": "$' + groupName + '", "grp2": "$' + groupName2 + '"}'
       filter += ',{"$group": { "_id":' + grpids + ', "count": { "$sum": 1 }}}';
       filter += ',{"$group": { "_id": "$_id.grp1" , "count": { "$push": {"grp2" : "$_id.grp2","count" : "$count" } }}}';
@@ -317,33 +316,43 @@ export default class FilterStore {
   ///////////////////////////////////////////////////////////////////////////////////////////////////
   // filter actions
   ///////////////////////////////////////////////////////////////////////////////////////////////////
+  @observable
+  useLocalDb = true;
+
   @action
   submitFilter = () => {
+    this.submintMainDataFilter();
+    this.submitCityNameAndLocation();
+    this.submitGroupByYears();
+    this.submitfilterdGroupByYears();
+    this.submitfilterdGroup(this.groupBy);
+    this.submitfilterdGroup2(this.groupBy, this.groupBy2.name);
+  }
+  submintMainDataFilter = () => {
     this.isLoading = true;
     let filter = this.getFilter();
     console.log(filter)
-    if (this.cities.length >= 1) {
-      let city = this.cities[0];
-      var srvCity = new CityService();
-      srvCity.getCityByNameHe(city, this.updateLocation);
-    }
     fetchFilter(filter)
       .then((data: any[] | undefined) => {
         if (data !== null && data !== undefined) {
           this.markers = data;
+          //write Data to local db
+          setMulti(data);
         }
         this.isLoading = false;
-        if (this.cities.length >= 1)
-          this.cityResult = this.cities[0];
-        else
-          this.cityResult = "";
       })
-    this.submitGroupByYears();
-    this.submitfilterdGroupByYears();
-    this.submitfilterdGroup(this.groupBy);
-    this.submitfilterdGroup2(this.groupBy,  this.groupBy2.name);
-    demoIDb(); 
   }
+  submitCityNameAndLocation = () => {
+    if (this.cities.length >= 1) {
+      let city = this.cities[0];
+      var srvCity = new CityService();
+      srvCity.getCityByNameHe(city, this.updateLocation);
+      this.cityResult = this.cities[0];
+    }
+    else
+      this.cityResult = "";
+  }
+
   getFilter = () => {
     let filter = `{"$and" : [`
     filter += `{"accident_year":  { "$gte" : "${this.startYear}","$lte": "${this.endYear}"}}`;
@@ -359,10 +368,10 @@ export default class FilterStore {
     filter += this.getMultiplefilter("accident_type_hebrew", this.accidentType);
     filter += this.getMultiplefilter("vehicle_vehicle_type_hebrew", this.vehicleType);
     filter += this.getMultiplefilter("road_type_hebrew", this.roadTypes);
-    filter += this.getMultiplefilter("speed_limit_hebrew",this.speedLimit);
-    filter += this.getMultiplefilter("road_width_hebrew",this.roadWidth);
-    filter += this.getMultiplefilter("multi_lane_hebrew",this.separator);
-    filter += this.getMultiplefilter("one_lane_hebrew",this.oneLane);
+    filter += this.getMultiplefilter("speed_limit_hebrew", this.speedLimit);
+    filter += this.getMultiplefilter("road_width_hebrew", this.roadWidth);
+    filter += this.getMultiplefilter("multi_lane_hebrew", this.separator);
+    filter += this.getMultiplefilter("one_lane_hebrew", this.oneLane);
     filter += `]}`
     return filter;
   }
@@ -425,12 +434,11 @@ export default class FilterStore {
       filter += arrfilter.map((x: string) => {
         if (x === "null")
           return `{"${filterKey}":` + null + '}'
-        else
-          {
-            let xSafe = x.replace('"', '\\"')
-            return `{"${filterKey}" : "${xSafe}"}`
-          }
-         
+        else {
+          let xSafe = x.replace('"', '\\"')
+          return `{"${filterKey}" : "${xSafe}"}`
+        }
+
       }
       ).join(',')
       filter += `]}`
