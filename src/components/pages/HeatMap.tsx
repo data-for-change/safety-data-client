5e3d7cfca0736b666f16bacc2432b93a786a0998
citizen-dror <reshef.dror@gmail.com>
2020-03-25 07:34:36 +0200

    filterd heat map

--- src/components/pages/HeatMap.tsx
+++ src/components/pages/HeatMap.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React ,{FunctionComponent} from 'react';
 import { Map, TileLayer } from 'react-leaflet';
-import {LeafletMouseEvent} from 'leaflet'
+import { useStore } from '../../stores/storeConfig'
+import { observer } from "mobx-react"
+import { toJS } from 'mobx'
 import 'leaflet-css'
 // @ts-ignore
 import HeatmapLayer from 'react-leaflet-heatmap-layer';
-import {getAll} from '../../services/Accident.Service'
+//import {getAll} from '../../services/Accident.Service'
  
-
-
 
 interface IPoint{
   lat: number,
@@ -19,74 +19,72 @@ interface IState {
   fitBoundsOnUpdate?: boolean;
   addressPoints?: Array<IPoint>;
 }
-
+interface IProps {}
 const INITIAL_ZOOM = 10;
-
-export default class HeatMap extends React.Component<{},IState> {
-    public state = {
-      layerHidden: false,
-      fitBoundsOnUpdate : true,
-      addressPoints: []
-    };
+const HeatMap: FunctionComponent<IProps> = observer(() => {
+  const WRAPPER_STYLES = { height: '80vh', width: '100vw', maxWidth: '100%' };
+  const store = useStore(); 
+  let reactMarkers = toJS(store.dataAllInjuries);
+  let newArr:any[] = reactMarkers.map (x => [x.latitude, x.longitude, x._id])
+  return (
+    <div>
+      <Map center={[32.09, 34.7818]} zoom={INITIAL_ZOOM} style={WRAPPER_STYLES}
+      
+      >
+      {true &&
+        <HeatmapLayer
+          fitBoundsOnLoad
+          fitBoundsOnUpdate
+          points={newArr}
+          longitudeExtractor={(m:any) => m[1]}
+          latitudeExtractor={(m:any) => m[0]}
+          intensityExtractor={(m:any) => parseFloat(m[2])} />
+          }
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </Map>
+      {/* <button className="button" onClick={() => this.setState({ layerHidden: !this.state.layerHidden })}>Toggle Layer</button>
+      <button className="button" type="button" onClick={this.getPoints} >Add Heat Points</button> */}
+    </div>
+)})
+export default HeatMap
+// export default class HeatMap extends React.Component<{},IState> {
+//     public state = {
+//       layerHidden: false,
+//       fitBoundsOnUpdate : true,
+//       addressPoints: []
+//     };
   
-  componentWillMount() {
-/*     const { addressPoints } = this.state
-    geojsonPoints.data.map ((x:any)=>{return addressPoints.push( [x.latitude,x.longitude,x.accident_timestamp]) })
-    // {{latitude: number, longitude: number,accident_timestamp: string}}
-    this.setState({ addressPoints }) */
-    console.log("heatmap")
-  }
-  getPoints =(e:object) =>{
-   
-    getAll(this.addPointsToMap);
-  }
-  addPoint = (e:LeafletMouseEvent) => {
-    let newPoint:any = [e.latlng.lat,e.latlng.lng,e.originalEvent.timeStamp.toString()]
-    if (this.state.addressPoints.length >0)
-    {
-      this.setState(prevState => ({
-        addressPoints: [...prevState.addressPoints, newPoint]
-      }))
-    }
-    // else{
-    //   this.setState(state => ({
-    //     addressPoints: [newPoint]
-    //   }))
-    // }
-  }
-  addPointsToMap=(arrPoints: any[]) =>{
-    let newArr:any[] = arrPoints.map (x => [x.latitude, x.longitude, x._id])
-    this.setState({ addressPoints: [...newArr]});
-  }
-  render() {
-    const WRAPPER_STYLES = { height: '400px', width: '100vw' }
+//     const WRAPPER_STYLES = { height: '400px', width: '100vw' }
 
-    return (
-      <div>
-        <Map center={[32.09, 34.7818]} zoom={INITIAL_ZOOM} style={WRAPPER_STYLES}
-         onClick={this.addPoint}
-        >
-        {!this.state.layerHidden &&
-          <HeatmapLayer
-            fitBoundsOnLoad
-            fitBoundsOnUpdate
-            points={this.state.addressPoints}
-            longitudeExtractor={(m:any) => m[1]}
-            latitudeExtractor={(m:any) => m[0]}
-            intensityExtractor={(m:any) => parseFloat(m[2])} />
-            }
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          />
-        </Map>
-        <button className="button" onClick={() => this.setState({ layerHidden: !this.state.layerHidden })}>Toggle Layer</button>
-        <button className="button" type="button" onClick={this.getPoints} >Add Heat Points</button>
-      </div>
-    );
-  }
+//     return (
+//       <div>
+//         <Map center={[32.09, 34.7818]} zoom={INITIAL_ZOOM} style={WRAPPER_STYLES}
+        
+//         >
+//         {!this.state.layerHidden &&
+//           <HeatmapLayer
+//             fitBoundsOnLoad
+//             fitBoundsOnUpdate
+//             points={this.state.addressPoints}
+//             longitudeExtractor={(m:any) => m[1]}
+//             latitudeExtractor={(m:any) => m[0]}
+//             intensityExtractor={(m:any) => parseFloat(m[2])} />
+//             }
+//           <TileLayer
+//             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//           />
+//         </Map>
+//         <button className="button" onClick={() => this.setState({ layerHidden: !this.state.layerHidden })}>Toggle Layer</button>
+//         <button className="button" type="button" onClick={this.getPoints} >Add Heat Points</button>
+//       </div>
+//     );
+  
  
-}
+// }
 
 export const geojsonPoints:any = {
   data: [
