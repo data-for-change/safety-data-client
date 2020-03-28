b6e4e00452704ebfcfa55cde41d9f8e5432ed527
citizen-dror <reshef.dror@gmail.com>
2020-03-28 22:47:38 +0300

    base for validtion group chekbox

--- src/components/molecules/GroupCheckBox.tsx
+++ src/components/molecules/GroupCheckBox.tsx
@@ -1,5 +1,5 @@
 
-import React, { ChangeEvent } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import { useTranslation } from 'react-i18next';
 import { observer } from "mobx-react"
 import Form from 'react-bootstrap/Form';
@@ -11,17 +11,37 @@ interface IProps {
   groupName: string,
   dataArr: Array<IFilterChecker>,
   onChange: (aType: number, val: boolean) => void
+
 }
 
 export const GroupCheckbox: React.FC<IProps> = observer(({ formName, groupName, dataArr, onChange }) => {
+  const styleFeedback = {
+    marginTop: '.25rem',
+    fontSize: '80%',
+    color: '#dc3545'
+  };
   const { t } = useTranslation();
-  //const store = useStore();
+  const [count, setCount] = useState(dataArr.length);
+  const checkValid = (checked: boolean) => {
+    if (checked)
+      setCount(count + 1)
+    else
+      setCount(count - 1)
+  }
+  const onGroupChange = (aType: number, checked: boolean) => {
+    checkValid(checked)
+    onChange(aType, checked);
+  }
+  const feedback = <div style={styleFeedback}> {t('check_one_checkbox')}</div>
+  const isvalid = (count === 0) ? feedback : null;
+
   return (
     <Form.Group controlId={formName + ".Control" + groupName} >
       <Form.Label className="filterLable">{t(groupName)}:</Form.Label>
       {dataArr.map((fChecker, index) => (
-        <Checkbox key={index} label={fChecker.label} group={groupName} id={index} checked={fChecker.checked} onChange={(e: ChangeEvent<HTMLInputElement>) => { onChange(index, e.target.checked); }} />
+        <Checkbox key={index} label={fChecker.label} group={groupName} id={index} checked={fChecker.checked} onChange={(e: ChangeEvent<HTMLInputElement>) => { onGroupChange(index, e.target.checked); }} />
       ))}
+      {isvalid}
     </Form.Group>
   )
 })
