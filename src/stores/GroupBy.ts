e409357106a0a4b29c68c1977d6d3613d68fe205
citizen-dror <reshef.dror@gmail.com>
2020-03-30 15:30:41 +0300

    remove month from groupby

--- src/stores/GroupBy.ts
+++ src/stores/GroupBy.ts
@@ -25,7 +25,7 @@ export const initGroupByDict = (dictGroupBy: any) => {
   dictGroupBy["Gender"] = new GroupBy('Gender', "sex_hebrew");
   dictGroupBy["Age"] = new GroupBy('Age', "age_group_hebrew");
   dictGroupBy["Year"] = new GroupBy('Year', "accident_year");
-  dictGroupBy["Month"] = new GroupBy('Month', "accident_month");
+  //dictGroupBy["Month"] = new GroupBy('Month', "accident_month");
   dictGroupBy["DayNight"] = new GroupBy('DayNight', "day_night_hebrew");
   dictGroupBy["WeekDay"] = new GroupBy('WeekDay', "day_in_week_hebrew");
   dictGroupBy["RoadType"] = new GroupBy('RoadType', "road_type_hebrew");
