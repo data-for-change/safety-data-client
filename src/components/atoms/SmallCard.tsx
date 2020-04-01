517295b97976772ffd05698ce430348fd9080580
citizen-dror <reshef.dror@gmail.com>
2020-04-01 17:22:29 +0300

    improve style card

--- src/components/atoms/SmallCard.tsx
+++ src/components/atoms/SmallCard.tsx
@@ -4,28 +4,27 @@ import Card from 'react-bootstrap/Card';
 interface IProps {
     title?: string,
     styleType?: number
-    width?:number
+    width?: number
 }
-export const SmallCard: React.FC<IProps> = ({ title, styleType=0,width=0,children }) => {
-    const style:any = [
-        { width: '15rem'}
-        ,{ width: '19rem'}
-        ,{ width: '25rem'}
-        ,{ width: '40rem'}
+export const SmallCard: React.FC<IProps> = ({ title, styleType = 0, width = 0, children }) => {
+    const styleWidth: any[] = [
+        '15rem', '20rem', '25rem', '40rem'
     ];
-    const stylebody = {
-        padding: "0.2rem"
-    }
+    let styleCard: any =
+    {
+        borderRadius: '5px',
+        padding: '5px',
+        margin: '7px'
+    };
     let aHeder = title ? <Card.Header>{title}</Card.Header> : "";
-    let aStyle= style[styleType];
-    if (width >0)
-        aStyle = {"width": width}
+    if (width > 0)
+        styleCard.width = width
+    else
+        styleCard.width = styleWidth[styleType]
     return (
-        <Card style={aStyle}>
-            <Card.Body style={stylebody}>
-                {aHeder}
-                {children}
-            </Card.Body>
+        <Card style={styleCard}>
+            {aHeder}
+            {children}
         </Card>
     )
 }
