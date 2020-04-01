7061256dd7f11cec7e9b308122889b83a1ef4508
citizen-dror <reshef.dror@gmail.com>
2020-04-01 21:51:44 +0300

    make all chars responsive

--- src/components/organisms/GroupByGraphsPanel.tsx
+++ src/components/organisms/GroupByGraphsPanel.tsx
@@ -12,8 +12,10 @@ import MyBarChart from '../molecules/MyBarChart'
 interface IProps { }
 const getSize = (width: number) => {
     let size: number = 500;
-    if (width <= 500)
-        size = 450;
+    if (width <= 350)
+        size = 300;
+    else if (width <= 760)
+        size = width * 0.85;
     else if (width <= 1500)
         size = width * 0.75;
     else
@@ -36,6 +38,46 @@ export const GroupByGraphsPanel: React.FC<IProps> = observer(() => {
         marginLeft: "20px",
         marginRight: "20px"
     };
+    React.useEffect(() => {
+        function handleResize() {
+            const size = getSize(window.innerWidth)
+            setGraphSize(size)
+        }
+        window.addEventListener('resize', handleResize)
+        return (() => { window.removeEventListener('resize', handleResize) })
+    })
+    let graph1Size = Math.min(380, graphSize)
+    let graph2Size = Math.min(600, graphSize)
+    //let width = window.innerWidth|| document.documentElement.clientWidth|| document.body.clientWidth;  
+    let reactData1 = toJS(store.dataByYears)
+    let reactData2 = toJS(store.dataFilterdByYears)
+    let reactData3 = toJS(store.dataFilterd)
+    let barsGrp2 = store.groupBy2.getBars();
+    let reactDataGrp2 = toJS(store.dataGroupby2)
+    if (reactData1.length > 0) {
+        return (
+            <div className="row" style={style}>
+                <SmallCard styleType={2} title={t('CasualtiesByFilter')}>
+                    <MyBarChart data={reactData2} width={graph1Size}  fill="#FE9772" />
+                </SmallCard>
+                <SmallCard styleType={3}>
+                    <SelectGroupBy id="Graphs.Main" />
+                    <MyBarChart data={reactData3} width={graph2Size} height={graph2Size*0.65}></MyBarChart>
+                </SmallCard>
+                <SmallCard width={graphSize + 150}>
+                    <div className="row">
+                        <span style={styleLable}> {t('GroupBy')}:</span>
+                        <SelectGroupBy id="Graphs.Grp2" labelText='' />
+                        <SelectGroupBy2 id="Graphs" />
+                        {/* <RangeSlider id="Graphs" label="resize" value={80} onChange={onSizeSliderChange}/> */}
+                    </div>
+                    <MyBarChart data={reactDataGrp2} bars={barsGrp2} width={graphSize} height={graphSize * 0.62} legendType="top"></MyBarChart>
+                </SmallCard>
+            </div>
+        )
+    }
+    else return null;
+})
     // const onSizeSliderChange = (event: ChangeEvent<HTMLInputElement>) => {
     //     let size:number = parseInt(event.target.value)
     //     switch(true) {
@@ -55,46 +97,7 @@ export const GroupByGraphsPanel: React.FC<IProps> = observer(() => {
     //             size= 500;
     //       }
     //     setGraphSize(size) 
-    //   }; 
-    React.useEffect(() => {
-        function handleResize() {
-            const size = getSize(window.innerWidth)
-            setGraphSize(size)
-        }
-        window.addEventListener('resize', handleResize)
-        return (() => { window.removeEventListener('resize', handleResize) })
-    })
-    //let width = window.innerWidth|| document.documentElement.clientWidth|| document.body.clientWidth;  
-    //const graphSize = getSize(width);
-    let reactData1 = toJS(store.dataByYears)
-    let reactData2 = toJS(store.dataFilterdByYears)
-    let reactData3 = toJS(store.dataFilterd)
-    let barsGrp2 = store.groupBy2.getBars();
-    let reactDataGrp2 = toJS(store.dataGroupby2)
-    if (reactData1.length > 0) {
-        return (
-            <div className="row" style={style}>
-                <SmallCard styleType={2} title={t('CasualtiesByFilter')}>
-                    <MyBarChart data={reactData2} fill="#FE9772" />
-                </SmallCard>
-                <SmallCard styleType={3}>
-                    <SelectGroupBy id="Graphs.Main" />
-                    <MyBarChart data={reactData3} width={600} height={390}></MyBarChart>
-                </SmallCard>
-                <SmallCard width={graphSize + 150}>
-                    <div className="row">
-                        <span style={styleLable}> {t('GroupBy')}:</span>
-                        <SelectGroupBy id="Graphs.Grp2" labelText='' />
-                        <SelectGroupBy2 id="Graphs" />
-                        {/* <RangeSlider id="Graphs" label="resize" value={80} onChange={onSizeSliderChange}/> */}
-                    </div>
-                    <MyBarChart data={reactDataGrp2} bars={barsGrp2} width={graphSize} height={graphSize * 0.62} legendType="top"></MyBarChart>
-                </SmallCard>
-            </div>
-        )
-    }
-    else return null;
-})
+    //   };
 
 // function debounce(fn:()=>void, ms:any ) {
 //     let timer :any
