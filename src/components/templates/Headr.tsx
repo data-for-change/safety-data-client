05bf741a3e629b4898e682dbe1e02c1e059d75a4
citizen-dror <reshef.dror@gmail.com>
2020-04-02 13:25:15 +0300

    responsive header

--- src/components/templates/Headr.tsx
+++ src/components/templates/Headr.tsx
@@ -1,40 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import {LanguageSelector} from '../molecules/LanguageSelector';
+import { LanguageSelector } from '../molecules/LanguageSelector';
 import { Link } from 'react-router-dom';
+import ButtonMenu from '../atoms/ButtonMenu'
+import logo from '../../assets/safety-logo.png';
+
 
 interface IProps {
   title: string
 }
 interface IProps { }
-export const Header: React.FC<IProps> = ({title}) =>{
+export const Header: React.FC<IProps> = ({ title }) => {
   const divStyle = {
     display: 'flex',
     flexFlow: 'row wrap'
   };
-  const hederTitle ={
+  const hederTitle = {
     fontSize: '30px',
     paddingLeft: '20px',
     paddingRight: '20px'
   }
   const { t } = useTranslation();
+  const [showNavMenu, setShowNavMenu] = useState(getShowNavMenu(window.innerWidth));
+  const navMenu = (showNavMenu) ? <div style={divStyle}>
+    <nav className="App-nav">
+      <Link to="/">{t('Home')}</Link>
+      <Link to="/city">{t('Cities')}</Link>
+      <Link to="/about">{t('About')}</Link>
+    </nav>
+  </div> : <ButtonMenu />;
+  React.useEffect(() => {
+        function handleResize() {
+            const show = getShowNavMenu(window.innerWidth)
+            setShowNavMenu(show)
+        }
+        window.addEventListener('resize', handleResize)
+        return (() => { window.removeEventListener('resize', handleResize) })
+    })
   return (
     <header className="App-header">
-    <h1 style={hederTitle}>
-      {title}
-    </h1>
-    <div style={divStyle}>
-      <nav  className= "App-nav">
-        <Link to="/">{t('Home')}</Link>
-        <Link to="/city">{t('Cities')}</Link>
-        <Link to="/about">{t('About')}</Link>
-      </nav>
-    </div>
-    <div className="languageSelector">
-      <LanguageSelector/>
-    </div>
-  </header> 
+      <div style={divStyle}>
+      <img src={logo} alt="Safety-Data logo" height="45" width="188" />
+      {navMenu}
+      </div>
+      <div className="languageSelector">
+        <LanguageSelector />
+      </div>
+    </header>
   );
 }
+const getShowNavMenu = (width: number) => {
+  let res = true;
+  if (width <= 530)
+    res = false;
+  return res;
+}
+
 export default Header;
- 
