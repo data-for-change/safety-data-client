52b7fc19e1c70a4c0f43c504681104bf0a5cc500
citizen-dror <reshef.dror@gmail.com>
2020-04-14 21:36:58 +0300

    split observers to small component

--- src/components/organisms/GroupByTablesPanel.tsx
+++ src/components/organisms/GroupByTablesPanel.tsx
@@ -9,14 +9,68 @@ import { SelectGroupBy } from '../atoms/SelectGroupBy'
 import { SelectGroupBy2 } from '../atoms/SelectGroupBy2'
 
 interface IProps { }
-export const GroupByTablesPanel: FunctionComponent<IProps> = observer(() => {
-    const { t } = useTranslation();
+export const GroupByTablesPanel: FunctionComponent<IProps> = () => {
     const style = {
         marginLeft: "0",
         marginRight: "0",
         marginTop: "20px"
     };
-    const divConstolsRow = {
+    return (
+        <div className="row" style={style}>
+            <GroupTablesYears />
+            <GroupTablesYears2 />
+            <GroupTablesFilter />
+            <GroupTables2Grp/>
+        </div>
+    )
+};
+
+
+const GroupTablesYears: FunctionComponent<IProps> = observer(() => {
+    const { t } = useTranslation();
+    const { filterStore } = useStore();
+    const { dataByYears } = filterStore;
+    let reactData1 = toJS(dataByYears);
+    if (reactData1.length > 0) {
+        return (
+            <SmallCard title={t('AllCasualtiesInRegion')}>
+                <GroupByTable data={reactData1} />
+            </SmallCard>
+        )
+    }
+    else return null;
+});
+const GroupTablesYears2: FunctionComponent<IProps> = observer(() => {
+    const { t } = useTranslation();
+    const { filterStore } = useStore();
+    const { dataFilterdByYears } = filterStore;
+    let reactData2 = toJS(dataFilterdByYears);
+    if (reactData2.length > 0) {
+        return (
+            <SmallCard title={t('CasualtiesByFilter')}>
+                <GroupByTable data={reactData2} />
+            </SmallCard>
+        )
+    }
+    else return null;
+});
+const GroupTablesFilter: FunctionComponent<IProps> = observer(() => {
+    const { filterStore } = useStore();
+    const { dataFilterd , groupBy} = filterStore;
+    const reactData = toJS(dataFilterd)
+    if (reactData.length > 0) {
+        return (
+            <SmallCard styleType={1}>
+                <SelectGroupBy id="Tables.Main" />
+                <GroupByTable data={reactData} dataName={groupBy.text} />
+            </SmallCard>
+        )
+    }
+    else return null;
+});
+
+const GroupTables2Grp: FunctionComponent<IProps> = observer(() => {
+    const divStyle = {
         display: "flex",
         flexWrap: "wrap"
     } as React.CSSProperties;
@@ -26,34 +80,20 @@ export const GroupByTablesPanel: FunctionComponent<IProps> = observer(() => {
         marginLeft: "20px",
         marginRight: "20px"
     };
+    const { t } = useTranslation();
     const { filterStore } = useStore();
-    const { dataByYears, dataFilterdByYears, dataFilterd } = filterStore;
-    let reactData1 = toJS(dataByYears);
-    let reactData2 = toJS(dataFilterdByYears);
-    let reactData3 = toJS(dataFilterd);
-    let columnsGrp2 = filterStore.groupBy2.getColumns().map((x) => { return ({ dataField: x, text: t(x) }) })
-    let reactDataGrp2 = toJS(filterStore.dataGroupby2)
-    if (reactData1.length > 0) {
+    const { groupBy, groupBy2, dataGroupby2} = filterStore;
+    const columnsGrp2 = groupBy2.getColumns().map((x) => { return ({ dataField: x, text: t(x) }) })
+    const reactDataGrp2 = toJS(dataGroupby2)
+    if (reactDataGrp2.length > 0) {
         return (
-            <div className="row" style={style}>
-                <SmallCard title={t('AllCasualtiesInRegion')}>
-                    <GroupByTable data={reactData1} />
-                </SmallCard>
-                <SmallCard title={t('CasualtiesByFilter')}>
-                    <GroupByTable data={reactData2} />
-                </SmallCard>
-                <SmallCard styleType={1}>
-                    <SelectGroupBy id="Tables.Main" />
-                    <GroupByTable data={reactData3} dataName={filterStore.groupBy.text} />
-                </SmallCard>
-                <SmallCard styleType={4}>
-                    <div style={divConstolsRow}>
-                        <span style={styleLable}> {t('GroupBy')}:</span>
-                        <SelectGroupBy id="Tables.Grp2" labelText='' />
-                        <SelectGroupBy2 id="Tables" /></div>
-                    <GroupByTable data={reactDataGrp2} dataName={filterStore.groupBy.text} columns={columnsGrp2} />
-                </SmallCard>
-            </div>
+            <SmallCard styleType={4}>
+                <div style={divStyle}>
+                    <span style={styleLable}> {t('GroupBy')}:</span>
+                    <SelectGroupBy id="Tables.Grp2" labelText='' />
+                    <SelectGroupBy2 id="Tables" /></div>
+                <GroupByTable data={reactDataGrp2} dataName={groupBy.text} columns={columnsGrp2} />
+            </SmallCard>
         )
     }
     else return null;
