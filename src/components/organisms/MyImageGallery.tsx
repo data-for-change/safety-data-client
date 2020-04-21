a5e16c844887607d055a3ffe39e7c6ece1a54032
citizen-dror <reshef.dror@gmail.com>
2020-04-21 16:30:42 +0300

    use react-image-gallery

--- src/components/organisms/MyImageGallery.tsx
+++ src/components/organisms/MyImageGallery.tsx
@@ -1,41 +1,34 @@
 import React, { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
+// import { useTranslation } from 'react-i18next';
 import { observer } from 'mobx-react';
 import { toJS } from 'mobx';
-import Image from 'react-bootstrap/Image';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
 import { useStore } from '../../stores/storeConfig';
 
 interface Props {
   type: string;
 }
 
-const ImageGallery: React.FC<Props> = observer(({ type }) => {
-  const { t } = useTranslation();
-  const { imageStore } = useStore();
+const MyImageGallery: React.FC<Props> = observer(({ type }) => {
+  const { imageStore, uiStore } = useStore();
   const { getImages } = imageStore;
+  const { language } = uiStore;
+  const isRTL = (language !== 'en');
   useEffect(() => {
     getImages(type);
   }, [getImages, type]);
   const arrayImagesProps = toJS(imageStore.imageList);
-  const reactGallery = arrayImagesProps.map((x: any) => (
-    <Col xs={6} md={4} key={`col-${x._id}`}>
-      <Image src={x.filename} thumbnail key={`img-${x._id}`} />
-      <div><b>{x.titlehe}</b></div>
-      <div>{x.texthe}</div>
-    </Col>
-  ));
+  const images = arrayImagesProps.map((x: any) => ({
+    original: x.filename,
+    thumbnail: x.filename,
+    originalTitle: x.titlehe,
+    description: x.texthe,
+  }));
   return (
     <div>
-      <h2>{t('Images')}</h2>
-      <Container>
-        <Row>
-          {reactGallery}
-        </Row>
-      </Container>
+      <ImageGallery items={images} isRTL={isRTL} />
     </div>
   );
 });
-export default ImageGallery;
+export default MyImageGallery;
