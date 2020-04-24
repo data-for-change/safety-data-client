0687192883491bec3e101b9276607d8f84bc975c
citizen-dror <reshef.dror@gmail.com>
2020-04-24 19:08:47 +0300

    gallery style

--- src/components/organisms/MyImageGallery.tsx
+++ src/components/organisms/MyImageGallery.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { observer } from 'mobx-react';
 import { toJS } from 'mobx';
+import Card from 'react-bootstrap/Card';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 import { useStore } from '../../stores/storeConfig';
@@ -9,6 +10,12 @@ import { useStore } from '../../stores/storeConfig';
 interface Props {
   type: string;
 }
+const styleCard: any = {
+  borderRadius: '5px',
+  padding: '5px',
+  margin: '7px',
+  width: '80%',
+};
 
 const MyImageGallery: React.FC<Props> = observer(({ type }) => {
   const { t } = useTranslation();
@@ -28,10 +35,10 @@ const MyImageGallery: React.FC<Props> = observer(({ type }) => {
   }));
   const isGotImages = (images.length > 0);
   return (
-    <div>
+    <Card style={styleCard}>
       {isGotImages && <ImageGallery items={images} isRTL={isRTL} />}
       {!isGotImages && `${t('not-found-images')}`}
-    </div>
+    </Card>
   );
 });
 export default MyImageGallery;
