3dea1cc20639f8a725785b1015dba29a72806677
citizen-dror <reshef.dror@gmail.com>
2020-04-26 13:51:33 +0300

    add tab select to image  gallery

--- src/components/organisms/MyImageGallery.tsx
+++ src/components/organisms/MyImageGallery.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { observer } from 'mobx-react';
 import { toJS } from 'mobx';
 import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ImageGallery from 'react-image-gallery';
@@ -22,13 +23,15 @@ const styleCard: any = {
 
 const MyImageGallery: React.FC<Props> = observer(({ type }) => {
   const { imageStore, uiStore } = useStore();
-  const { getImages, setCurrImage } = imageStore;
+  const {
+    getImages, setCurrImage, imageList, getImagesByTag,
+  } = imageStore;
   const { language } = uiStore;
   const isRTL = (language !== 'en');
   useEffect(() => {
     getImages(type);
   }, [getImages, type]);
-  const arrayImagesProps = toJS(imageStore.imageList);
+  const arrayImagesProps = toJS(imageList);
   const images = arrayImagesProps.map((x: any) => ({
     original: x.filename,
     thumbnail: x.filename,
@@ -36,26 +39,27 @@ const MyImageGallery: React.FC<Props> = observer(({ type }) => {
     description: x.texthe,
   }));
   const isGotImages = (images.length > 0);
-  const handleOnSlide = (currentIndex:number) => {
+  const handleOnSlide = (currentIndex: number) => {
     if (arrayImagesProps.length > 0) setCurrImage(arrayImagesProps[currentIndex]);
     return true;
   };
   return (
     <Card style={styleCard}>
       <Row>
-        <Col />
-        <Col xs={10}>
-          {' '}
+        <Col xs={3}>
+          <SelectImageByTag onChange={(val:string) => getImagesByTag(val)} />
+        </Col>
+        <Col xs={8}>
           {isGotImages && <ImageTitle />}
         </Col>
-        <Col>{isGotImages && <ButtonToggleHideDescription />}</Col>
+        <Col xs={1}>{isGotImages && <ButtonToggleHideDescription />}</Col>
       </Row>
       {isGotImages && (
-      <ImageGallery
-        items={images}
-        isRTL={isRTL}
-        onSlide={(currentIndex: number) => handleOnSlide(currentIndex)}
-      />
+        <ImageGallery
+          items={images}
+          isRTL={isRTL}
+          onSlide={(currentIndex: number) => handleOnSlide(currentIndex)}
+        />
       )}
       {!isGotImages && <ImageMsg />}
     </Card>
@@ -74,6 +78,36 @@ const ButtonToggleHideDescription: React.FC<{}> = observer(() => {
     />
   );
 });
+
+interface PropsSelectImageByTag {
+  onChange: (val:string) => void;
+}
+const styleSelect = {
+  width: '150px',
+};
+const SelectImageByTag: React.FC<PropsSelectImageByTag> = observer(({ onChange }) => (
+  <div>
+    <Form.Group as={Col} controlId="exampleForm.SelectTag">
+      {/* <Form.Label>
+        tags
+      </Form.Label> */}
+      <Form.Control
+        as="select"
+        style={styleSelect}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => { onChange(e.target.value); }}
+      >
+        <option>הולכי רגל</option>
+        <option>רוכבי אופניים</option>
+        <option>רוכבי אופנוע</option>
+        <option>מכוניות</option>
+        <option>אוטובוסים</option>
+        <option>משאיות</option>
+        <option>כללי</option>
+        <option>ילדים</option>
+      </Form.Control>
+    </Form.Group>
+  </div>
+));
 
 const styleTitle: React.CSSProperties = {
   textAlign: 'center',
@@ -94,7 +128,7 @@ const ImageMsg: React.FC<{}> = observer(() => {
   const { imageStore } = useStore();
   const { isLoading } = imageStore;
   return (
-    <span>{(isLoading) ? `${t('Loading…')}` : `${t('not-found-images')}` }</span>
+    <span>{(isLoading) ? `${t('Loading…')}` : `${t('not-found-images')}`}</span>
   );
 });
 
