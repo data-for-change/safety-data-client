1d25b0434a9a14266053b6578fe726e34e0b2bb2
ruslanNir <nirkuba19999@gmail.com>
2021-02-02 10:58:37 +0200

    loader comp

---
+++ src/components/atoms/Loader.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import Spinner from 'react-bootstrap/Spinner'
+
+const Loader: React.FC<any> = () => <div className="loader-con"> <Spinner animation="border" variant="info" /></div>
+
+export default Loader
