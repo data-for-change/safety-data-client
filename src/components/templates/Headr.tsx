0d44d2dba552cbc68cb420bcf84af7306ccf62f5
citizen-dror <reshef.dror@gmail.com>
2021-02-02 14:08:21 +0200

    eslint header

--- src/components/templates/Headr.tsx
+++ src/components/templates/Headr.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import LanguageSelector from '../molecules/LanguageSelector';
@@ -11,87 +11,59 @@ interface IProps {
    title: string
 }
 // interface IProps { }
-export const Header: React.FC<IProps> = ({ title }) => {
-   // const divStyle = {
-   //   display: 'flex',
-   //   flexFlow: 'row wrap',
-   // };
-   const { t } = useTranslation();
-   // const [showNavMenu, setShowNavMenu] = useState(getShowNavMenu(window.innerWidth));
-   // const navMenu = (
-   //   <div style={divStyle}>
-   //     <nav className="App-nav">
-   //       <Link to="/">{t('Home')}</Link>
-   //       <Link to="/city">{t('Cities')}</Link>
-   //       <Link to="/about">{t('About')}</Link>
-   //     </nav>
-   //   </div>
-   // )
-   // React.useEffect(() => {
-   //   function handleResize() {
-   //     const show = getShowNavMenu(window.innerWidth);
-   //     setShowNavMenu(show);
-   //   }
-   //   window.addEventListener('resize', handleResize);
-   //   return (() => { window.removeEventListener('resize', handleResize); });
-   // });
-   // {/* <div style={divStyle}>
-   //   <img src={logo} alt="Safety-Data logo" height="45" width="188" />
-   //   {navMenu}
-   // </div>
-   // <div className="languageSelector">
-   //   <LanguageSelector />
-   // </div> */}
-   return (
-      <header style={{ marginBottom: '1rem' }}>
-         <nav className="navbar navbar-expand-lg navbar-dark shadow">
-            <div className="container-fluid">
-               <img
-                  src={logo}
-                  alt="Safety-Data logo"
-                  height="45"
-                  width="188"
-               />
-               <button
-                  className="navbar-toggler active"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#navbarNavAltMarkup">
-                  <span className="navbar-toggler-icon"></span>
-               </button>
-               <div
-                  className="collapse navbar-collapse"
-                  id="navbarNavAltMarkup">
-                  <div className="navbar-nav">
-                     <Link
-                        className="nav-link"
-                        to="/">
-                        {t('Home')}
-                     </Link>
-                     <Link
-                        className="nav-link"
-                        to="/city">
-                        {t('Cities')}
-                     </Link>
-                     <Link
-                        className="nav-link"
-                        to="/about">
-                        {t('About')}
-                     </Link>
-                  </div>
-               </div>
-               <div>
-                  <LanguageSelector />
-               </div>
+export const Header: React.FC<IProps> = ({ title }:IProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <header style={{ marginBottom: '1rem' }}>
+      <nav className="navbar navbar-expand-lg navbar-dark shadow">
+        <div className="container-fluid">
+          <img
+            src={logo}
+            alt={`${title} logo`}
+            height="45"
+            width="188"
+          />
+          <button
+            className="navbar-toggler active"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div
+            className="collapse navbar-collapse"
+            id="navbarNavAltMarkup"
+          >
+            <div className="navbar-nav">
+              <Link
+                className="nav-link"
+                to="/"
+              >
+                {t('Home')}
+              </Link>
+              <Link
+                className="nav-link"
+                to="/city"
+              >
+                {t('Cities')}
+              </Link>
+              <Link
+                className="nav-link"
+                to="/about"
+              >
+                {t('About')}
+              </Link>
             </div>
-         </nav>
-      </header>
-   );
+          </div>
+          <div>
+            <LanguageSelector />
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 };
-// const getShowNavMenu = (width: number) => {
-//   let res = true;
-//   if (width <= 530) res = false;
-//   return res;
-// };
 
 export default Header;
