78cbf720d4d160583185c4e4ac0b2d6500670339
ruslanNir <nirkuba19999@gmail.com>
2021-02-04 13:48:34 +0200

    comment buttons

--- src/components/organisms/FilterPanel.tsx
+++ src/components/organisms/FilterPanel.tsx
@@ -8,48 +8,61 @@ import ConfigFilter from '../molecules/ConfigFilter';
 import gearlogo from '../../assets/gear2.png';
 
 interface IProps {
-  activeCardKey?: number
+   activeCardKey?: number
 }
 const styles = {
-  buttonStyle: {
-    display: 'flex',
-    justifyContent: 'space-between',
-  },
-  iconStyle: {
-    height: '21px',
-    width: '21px',
-    paddingTop: '0px',
-    paddingBottom: '1px',
-  },
+   buttonStyle: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      marginBottom: '1rem'
+   },
+   iconStyle: {
+      height: '21px',
+      width: '21px',
+      paddingTop: '0px',
+      paddingBottom: '1px',
+   },
 };
 
 const FilterPanel: React.FC<IProps> = ({ activeCardKey = 0 }: IProps) => {
-  const [open, setOpen] = useState(true);
-  const [showModel, setShowModal] = useState(false);
-  const { t } = useTranslation();
-  const showFilter = () => {
-    setOpen(!open);
-  };
-  const textHideButton = (open) ? t('HideFilter') : t('ShowFilter');
-  return (
-    <>
-      <div style={styles.buttonStyle}>
-        <Button onClick={() => { showFilter(); }} aria-controls="example-collapse-text" aria-expanded={open}>
-          {textHideButton}
-        </Button>
-        <Button onClick={() => { setShowModal(!showModel); }}>
-          <img src={gearlogo} alt="settings" style={styles.iconStyle} />
-        </Button>
-      </div>
-      <ConfigFilterModal title="Filter Options" showModal={showModel} setShow={setShowModal}>
-        <ConfigFilter />
-      </ConfigFilterModal>
-      <Collapse in={open}>
-        <div id="example-collapse-text">
-          <FilterRequest activeCardKey={activeCardKey} />
-        </div>
-      </Collapse>
-    </>
-  );
+   const [open, setOpen] = useState(true);
+   const [showModel, setShowModal] = useState(false);
+   const { t } = useTranslation();
+   const showFilter = () => {
+      setOpen(!open);
+   };
+   const textHideButton = (open) ? t('HideFilter') : t('ShowFilter');
+   return (
+      <React.Fragment>
+         {/* <div style={styles.buttonStyle}>
+            <Button
+               className="btn-sm"
+               onClick={() => { showFilter(); }}
+               aria-controls="example-collapse-text"
+               aria-expanded={open}>
+               {textHideButton}
+            </Button>
+            <Button onClick={() => { setShowModal(!showModel); }}>
+               <img
+                  src={gearlogo}
+                  alt="settings"
+                  style={styles.iconStyle}
+               />
+            </Button>
+         </div> */}
+         {/* <ConfigFilterModal
+            size="sm"
+            title="Filter Options"
+            showModal={showModel}
+            setShow={setShowModal}>
+            <ConfigFilter />
+         </ConfigFilterModal> */}
+         <Collapse in={open}>
+            <div id="example-collapse-text">
+               <FilterRequest activeCardKey={activeCardKey} />
+            </div>
+         </Collapse>
+      </React.Fragment>
+   );
 };
 export default FilterPanel;
