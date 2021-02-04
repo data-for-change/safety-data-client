ebcc6c7288ef55a733df206c00c4c8fdcb5fb6d5
citizen-dror <reshef.dror@gmail.com>
2021-02-04 11:30:33 +0200

    init unit-test jest

---
+++ src/components/atoms/__tests__/ButtonMenu.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+import ButtonMenu from '../ButtonMenu';
+
+it('ButtonMenu render no crash', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<ButtonMenu />, div);
+});
