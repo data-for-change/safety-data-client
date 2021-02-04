0316fecbd1e2a78311bf6be5070d079510c617b6
citizen-dror <reshef.dror@gmail.com>
2021-02-04 11:37:22 +0200

    unmountComponentAtNode

--- src/components/atoms/__tests__/ButtonMenu.test.js
+++ src/components/atoms/__tests__/ButtonMenu.test.js
@@ -6,4 +6,5 @@ import ButtonMenu from '../ButtonMenu';
 it('ButtonMenu render no crash', () => {
   const div = document.createElement('div');
   ReactDOM.render(<ButtonMenu />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
