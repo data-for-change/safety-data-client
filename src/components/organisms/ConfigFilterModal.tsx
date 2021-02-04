c05fce4cc683a95efbc6610d2547a4ec06bccc7e
ruslanNir <nirkuba19999@gmail.com>
2021-02-04 13:47:12 +0200

    modal add props: action,size

--- src/components/organisms/ConfigFilterModal.tsx
+++ src/components/organisms/ConfigFilterModal.tsx
@@ -2,33 +2,55 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { useStore } from '../../stores/storeConfig';
+import { observer } from 'mobx-react';
 
 interface IProps {
   title: string;
   showModal: boolean;
   setShow: (show: boolean) => void;
   children: any;
+  size?: "sm" | "lg" | "xl" | undefined
+  action?: (val?: any) => void
 }
 
-const ConfigFilterModal: React.FC<IProps> = ({
-  title, showModal, setShow, children,
+
+
+const ConfigFilterModal: React.FC<IProps> = observer(({
+  title, showModal, setShow, children, size, action
 }: IProps) => {
   const { t } = useTranslation();
   const handleClose = () => setShow(false);
+  const { filterStore } = useStore();
+
+  const submitChanges = action ?
+    <Button
+      variant="primary"
+      onClick={action}
+      disabled={filterStore.isLoading || !filterStore.isValidAllFilters}
+    >
+      {filterStore.isLoading ? t('Loading…') : t('Submit')}
+    </Button>
+    : null
+
   return (
-    <Modal show={showModal} onHide={handleClose}>
+    <Modal
+      size={size}
+      show={showModal}
+      onHide={handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>{t(title)}</Modal.Title>
       </Modal.Header>
-      <Modal.Body>
+      <Modal.Body style={{ maxHeight: 'calc(100vh - 210px)', overflowY: 'auto' }}>
         {children}
       </Modal.Body>
       <Modal.Footer>
+        {submitChanges}
         <Button variant="secondary" onClick={handleClose}>
           {t('Close')}
         </Button>
       </Modal.Footer>
     </Modal>
   );
-};
+});
 export default ConfigFilterModal;
