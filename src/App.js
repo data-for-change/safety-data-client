91c6ba7068927919e005ae7c3c03410e4db99b58
ruslanNir <nirkuba19999@gmail.com>
2021-02-04 12:57:09 +0200

    styles global

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import CityPage from './components/pages/CityPage';
 import Header from './components/templates/Headr';
-// import Card from 'react-bootstrap/Card';
 import Footer from './components/templates/Footer';
 import './i18n';
 import './App.css';
 import Loader from './components/atoms/Loader';
-// import mapStore from './stores/MapStore';
 
 const AboutPage = lazy(() => import('./components/pages/AboutPage'));
 const UpdateImagePage = lazy(() => import('./components/pages/UpdateImagePage'));
