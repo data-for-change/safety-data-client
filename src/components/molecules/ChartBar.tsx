28ad44bae4ca1c70e1d0b34206e902f56c1b9774
citizen-dror <reshef.dror@gmail.com>
2021-02-07 19:54:17 +0200

    PieChart color list

--- src/components/molecules/ChartBar.tsx
+++ src/components/molecules/ChartBar.tsx
@@ -14,13 +14,16 @@ const ChartBar: React.FC<IProps> = observer(({ data, chartType = 'BarChart', hei
   const labels = data.map((x) => x._id);
   const vals = data.map((x) => x.count);
   const label = t('casualties');
+  let backgroundColor = (chartType === 'PieChart') ? 
+      ['#08367C','#214B8E','#395F9F','#5274B1','#6A89C2','#839DD4','#9BB2E5','#07681F','#2D7B2C','#538E39','#7AA146','#A0B352','#C6C65F','#ECD96C']
+  : fill;
   const data3 = {
     labels,
     datasets: [
       {
         label,
-        backgroundColor: fill,
-        borderColor: 'rgba(255,99,132,1)',
+        backgroundColor: backgroundColor,
+        borderColor: 'rgba(255,255,255,1)',
         borderWidth: 1,
         hoverBackgroundColor: 'rgba(255,99,132,0.4)',
         hoverBorderColor: 'rgba(255,99,132,1)',
