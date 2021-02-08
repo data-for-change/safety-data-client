9727dfcc054a8d02ee57b729df0146b04f9fc55a
citizen-dror <reshef.dror@gmail.com>
2021-02-08 18:52:20 +0200

    eslint

--- src/components/molecules/ChartBar.tsx
+++ src/components/molecules/ChartBar.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback} from 'react';
+import React from 'react';
 import { Bar, HorizontalBar, Pie } from 'react-chartjs-2';
 import { observer } from 'mobx-react';
 import { useTranslation } from 'react-i18next';
