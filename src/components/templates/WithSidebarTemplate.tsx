d576a20cb37534466e6989f8d7a9bda2222db7e3
ruslanNir <nirkuba19999@gmail.com>
2021-02-18 14:58:00 +0200

    add component whit side bar

---
+++ src/components/templates/WithSidebarTemplate.tsx
@@ -0,0 +1,19 @@
+import React, { PropsWithChildren } from 'react'
+import Drawer from '../organisms/Drawer'
+
+interface Props extends PropsWithChildren<any> { }
+
+const WithSidebarTemplate: React.FC<Props> = ({ children }) => {
+   return (
+      <div className="grid-with-drawer">
+         <div className="grid-drawer">
+            <Drawer />
+         </div>
+         <div className="grid-main">
+            {children}
+         </div>
+      </div>
+   )
+}
+
+export default WithSidebarTemplate
