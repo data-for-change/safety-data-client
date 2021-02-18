ab82af6f905e815d054cad3988de8c9a99c0af60
ruslanNir <nirkuba19999@gmail.com>
2021-02-18 14:58:13 +0200

    drawer component

---
+++ src/components/organisms/Drawer.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next';
+import FilterForm from './FilterForm'
+import { useStore } from '../../stores/storeConfig';
+import { observer } from 'mobx-react';
+import { Button } from 'react-bootstrap';
+import SmallCard2 from '../atoms/SmallCard2';
+
+const Drawer: React.FC<any> = observer((props) => {
+   // console.log('Drawer')
+   const { t } = useTranslation();
+   const { filterStore } = useStore();
+
+   return (
+      // <SmallCard2>
+      <div className="sidebar">
+         <div className="filters">
+            <FilterForm />
+         </div>
+         <Button
+            style={{ margin: '0.5rem' }}
+            variant="light"
+            onClick={() => filterStore.submitFilter()}
+            disabled={filterStore.isLoading || !filterStore.isValidAllFilters}
+         >
+            {filterStore.isLoading ? t('Loading') : t('Submit')}
+         </Button>
+      </div>
+   )
+   {/* </SmallCard2> */ }
+})
+
+
+export default Drawer
