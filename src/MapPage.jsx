a7195d8c59b36fec7481850eb1d090b28f397ba1
citizen-dror <reshef.dror@gmail.com>
2024-12-05 15:00:21 +0200

    add marker to map

--- src/MapPage.jsx
+++ src/MapPage.jsx
@@ -1,6 +1,19 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import markerIcon from './assets/marker-icon.png.png'
 
+const iconSize = {
+    iconSize: [25, 41], iconAnchor: [12, 41], popupAnchor: [1, -34], shadowSize: [41, 41],
+  };
+const blue_ICON = new L.Icon({
+    iconUrl: markerIcon,
+    iconSize: L.point(iconSize.iconSize[0], iconSize.iconSize[1]),
+    iconAnchor: L.point(iconSize.iconAnchor[0], iconSize.iconAnchor[1]),
+    popupAnchor: L.point(iconSize.popupAnchor[0], iconSize.popupAnchor[1]),
+    shadowSize: L.point(iconSize.shadowSize[0], iconSize.shadowSize[1]),
+  }); 
+  
 function MapPage() {
     const position = [51.505, -0.09];
     return (
@@ -12,6 +25,11 @@ function MapPage() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+                 <Marker position={position} icon={blue_ICON}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                 </Marker>
             </MapContainer>
         </div>
         </>
