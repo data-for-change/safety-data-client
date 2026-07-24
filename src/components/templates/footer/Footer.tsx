8f159853acedfeba2f7f2839e41606adc60a3f64
citizen-dror <reshef.dror@gmail.com>
2026-07-24 17:46:18 +0300

    style footer for mobile, version update

--- src/components/templates/footer/Footer.tsx
+++ src/components/templates/footer/Footer.tsx
@@ -1,55 +1,33 @@
-import React, { CSSProperties } from 'react';
+import React from 'react';
 import logoNatun from '../../../assets/logo/natun-logo.png';
 import logoAnyway from '../../../assets/logo/anyway-logo.png';
 import './footer.css';
 
-interface IProps {}
+interface IProps { }
 
 export const Footer: React.FC<IProps> = () => {
-  const footerStyle: CSSProperties = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    height: '4rem',
-    padding: '1rem',
-  };
-
-  const logoGroupStyle: CSSProperties = {
-    display: 'flex',
-    gap: '1rem',
-    alignItems: 'center',
-  };
-
-  const imgStyle: CSSProperties = {
-    height: '30px',
-  };
-
-  const centerTextStyle: CSSProperties = {
-    fontSize: '14px',
-    paddingTop: '5px',
-    textAlign: 'center',
-    flex: 1,
-  };
-
-  const versionStyle: CSSProperties = {
-    fontSize: '12px',
-    color: '#666',
-  };
-
-  const versionLabel = 'ver: 15/07/2026';
+ 
+  const versionLabel = 'ver: 24/07/2026';
 
   return (
-    <footer style={footerStyle}>
-      <div style={logoGroupStyle}>
+    <footer className="footer">
+      <div className="footer-logos">
         <a href="https://www.natoon.co.il/" title="מבית נתון לשינוי">
-          <img src={logoNatun} alt="Natun logo" style={imgStyle} />
+          <img src={logoNatun} alt="Natun logo" />
         </a>
+
         <a href="https://www.anyway.co.il/" title="anyway.co.il">
-          <img src={logoAnyway} alt="Anyway logo" style={imgStyle} />
+          <img src={logoAnyway} alt="Anyway logo" />
         </a>
       </div>
-      <span style={centerTextStyle}>פותח בשיתוף המטה לבטיחות בדרכים בעיריית תל אביב-יפו</span>
-      <span style={versionStyle}>{versionLabel}</span>
+
+      <div className="footer-text">
+        פותח בשיתוף המטה לבטיחות בדרכים בעיריית תל אביב-יפו
+      </div>
+
+      <div className="footer-version">
+        {versionLabel}
+      </div>
     </footer>
   );
 };
