1ec4042d9451608fa41a009306c13dca87a7e38c
citizen-dror <reshef.dror@gmail.com>
2026-07-28 06:56:09 +0300

    version 28/07/2026

--- src/components/templates/footer/Footer.tsx
+++ src/components/templates/footer/Footer.tsx
@@ -7,7 +7,7 @@ interface IProps { }
 
 export const Footer: React.FC<IProps> = () => {
  
-  const versionLabel = 'ver: 25/07/2026';
+  const versionLabel = 'ver: 28/07/2026';
 
   return (
     <footer className="footer">
